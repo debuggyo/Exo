520da95d0972ac208273205d62a659cf3a92ea79
debuggy <96699361+debuggyo@users.noreply.github.com>
2025-08-24 10:19:35 +1200

    clear for quickshell rewrite

--- scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-from .wallpaper import Wallpaper
-from .set_bar_styles import BarStyles
-from .send_notification import send_notification
-
-__all__ = [
-    "Wallpaper",
-    "BarStyles",
-    "send_notification",
-]
--- scripts/recorder.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from ignis.services.recorder import RecorderService, RecorderConfig
-
-recorder = RecorderService.get_default()
-
-rec_config = RecorderConfig(
-    source="portal",
-    path="path/to/file",
-)
