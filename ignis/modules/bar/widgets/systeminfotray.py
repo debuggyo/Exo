9a31a63bf6c6bcab7bab854a18446d10d724388b
debuggy <96699361+debuggyo@users.noreply.github.com>
2025-09-23 12:30:28 +1200

    date on vertical bar

--- ignis/modules/bar/widgets/systeminfotray.py
+++ ignis/modules/bar/widgets/systeminfotray.py
@@ -96,12 +96,18 @@ class Tray(widgets.Box):
 
 class Clock:
     def __init__(self):
-        self.container = widgets.Box(css_classes=["timedate"])
-        self.time_label = widgets.Label(css_classes=["time"])
-        self.date_label = widgets.Label(css_classes=["date"])
+        self.container = widgets.Box(css_classes=["timedate"], hexpand=True, vexpand=True)
+        self.time_label = widgets.Label(css_classes=["time"], justify="center")
+        self.separator = widgets.Label(css_classes=["separator"], visible=False, label="•", hexpand=False, vexpand=False)
+        self.date_label = widgets.Label(css_classes=["date"], justify="center")
+        self.date_separator = widgets.Separator(vertical=False, hexpand=False, vexpand=False)
+        self.month_label = widgets.Label(css_classes=["month"], justify="center", visible=False)
 
         self.container.append(self.time_label)
+        self.container.append(self.separator)
         self.container.append(self.date_label)
+        self.container.append(self.date_separator)
+        self.container.append(self.month_label)
 
         utils.Poll(1000, lambda _: self.update_labels())
 
@@ -113,17 +119,15 @@ class Clock:
         military_time = user_settings.interface.bar.modules.military_time
 
         if is_vertical:
-            time_format = "%H" if military_time else "%I"
-            date_format = "%M"
+            time_format = "%H%n%M" if military_time else "%I%n%M"
+            date_format = "%d"
         else:
-            if military_time:
-                time_format = "%H:%M"
-            else:
-                time_format = "%I:%M %p"
+            time_format = "%H:%M" if military_time else "%I:%M %p"
             date_format = "%a %d %b"
 
         self.time_label.set_label(now.strftime(time_format))
         self.date_label.set_label(now.strftime(date_format))
+        self.month_label.set_label(now.strftime("%m"))
 
     def update_layout(self):
         is_vertical = user_settings.interface.bar.vertical
@@ -133,17 +137,21 @@ class Clock:
         self.container.set_valign("fill")
 
         self.container.set_vertical(is_vertical)
-        if is_vertical:
-            self.container.set_hexpand(True)
-            self.container.set_vexpand(True)
+
+        if is_vertical and compact_mode < 3:
+            self.separator.set_visible(True)
+            self.date_label.set_visible(True)
+            self.date_separator.set_visible(True)
+            self.month_label.set_visible(True)
         else:
-            self.container.set_hexpand(True)
-            self.container.set_vexpand(True)
+            self.separator.set_visible(False)
+            self.date_label.set_visible(False)
+            self.date_separator.set_visible(False)
+            self.month_label.set_visible(False)
 
         if is_vertical:
-            self.date_label.set_visible(True)
             self.container.set_spacing(0)
-            self.container.set_homogeneous(True)
+            self.container.set_homogeneous(False)
             self.time_label.set_valign("center")
 
         elif compact_mode == 0:
@@ -153,6 +161,7 @@ class Clock:
             self.time_label.set_valign("end")
             self.date_label.set_valign("start")
             self.container.set_vertical(True)
+            self.separator.set_visible(False)
 
         elif compact_mode == 1:
             self.date_label.set_visible(True)
@@ -161,6 +170,7 @@ class Clock:
             self.time_label.set_valign("center")
             self.date_label.set_valign("center")
             self.container.set_vertical(False)
+            self.separator.set_visible(True)
 
         elif compact_mode == 2:
             self.date_label.set_visible(True)
@@ -169,6 +179,7 @@ class Clock:
             self.time_label.set_valign("center")
             self.date_label.set_valign("center")
             self.container.set_vertical(False)
+            self.separator.set_visible(True)
 
         elif compact_mode == 3:
             self.date_label.set_visible(False)
@@ -176,6 +187,7 @@ class Clock:
             self.container.set_homogeneous(False)
             self.time_label.set_valign("center")
             self.container.set_vertical(False)
+            self.separator.set_visible(False)
 
     def widget(self):
         return self.container
