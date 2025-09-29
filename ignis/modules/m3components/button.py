76be67fee524aab67299cf86250ef8d4ab721b9f
debuggy <96699361+debuggyo@users.noreply.github.com>
2025-09-29 16:55:36 +1300

    fix settings rows being bigger than they should on certain devices/fonts

--- ignis/modules/m3components/button.py
+++ ignis/modules/m3components/button.py
@@ -1,5 +1,6 @@
 from ignis import widgets
 
+
 class Button(widgets.Button):
     def set_icon(self, icon: str):
         child_box = self.get_child()
@@ -21,7 +22,7 @@ class Button(widgets.Button):
         css_classes: list = None,
         ialign: str = "center",
         vertical: bool = False,
-        **kwargs
+        **kwargs,
     ):
         if on_click is None:
             on_click = lambda *_: None
@@ -32,19 +33,20 @@ class Button(widgets.Button):
 
         children = []
         if icon:
-            children.append(widgets.Label(label=icon, css_classes=["m3-button-icon"], halign="center", hexpand=False))
+            children.append(
+                widgets.Label(
+                    label=icon,
+                    css_classes=["m3-button-icon"],
+                    halign="center",
+                    hexpand=False,
+                )
+            )
             if not label:
                 classes.append("icon-only")
         if label:
             children.append(widgets.Label(label=label, css_classes=["m3-button-label"]))
 
-        gap = {
-            "xs": 8,
-            "s": 8,
-            "m": 8,
-            "l": 12,
-            "xl": 16
-        }.get(size, 8)
+        gap = {"xs": 8, "s": 8, "m": 8, "l": 12, "xl": 16}.get(size, 8)
 
         return Button(
             css_classes=classes,
@@ -54,16 +56,18 @@ class Button(widgets.Button):
                 spacing=gap,
                 child=children,
                 halign=ialign,
+                valign="center",
             ),
-            **kwargs
+            **kwargs,
         )
 
     @staticmethod
     def connected_group(child, **kwargs):
         return widgets.Box(
             vertical=False,
+            vexpand=False,
             spacing=2,
             css_classes=["connected-button-group"],
             child=child,
-            **kwargs
+            **kwargs,
         )
